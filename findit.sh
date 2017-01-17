a9d398110dd188f7efaf3b59025d1a1b727608ae
Peter Nugent <nugent68@berkeley.edu>
2017-01-17 15:54:32 -0800

    Added in the homework
    author fix

--- findit.sh
+++ findit.sh
@@ -1,5 +1,14 @@
 #/bin/bash
 
+# Here's an example code to play with/re-write to do the first homework assignment.
+#
+# In /project/projectdirs/astro250/data we have a set of fits files as well as catalogs
+# from these fits files. They are named the same save for the suffix (.fits or .cat). 
+# Some of the .cat files are missing. Write a bash shell script which takes as input 
+# the directory name and prints out the names of the .fits files which are missing their 
+# corresponding .cat files.
+#
+
 dir=$1
 
 file=junk.fits
