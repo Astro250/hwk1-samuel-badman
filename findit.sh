8defa58414189077d014a66e471b1ae8784924b8
Peter Nugent <nugent68@berkeley.edu>
2017-01-17 15:57:00 -0800

    fix dir

--- findit.sh
+++ findit.sh
@@ -2,7 +2,7 @@
 
 # Here's an example code to play with/re-write to do the first homework assignment.
 #
-# In /project/projectdirs/astro250/data we have a set of fits files as well as catalogs
+# In /project/projectdirs/astro250/data/hwk1 we have a set of fits files as well as catalogs
 # from these fits files. They are named the same save for the suffix (.fits or .cat). 
 # Some of the .cat files are missing. Write a bash shell script which takes as input 
 # the directory name and prints out the names of the .fits files which are missing their 
