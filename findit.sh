8c693bccfd684cd2b2a9ce74a2855aeb509014b4
Peter Nugent <nugent68@berkeley.edu>
2017-02-13 09:56:49 -0800

    Tweaked the directory

--- findit.sh
+++ findit.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-# Here's the first line of code to play write to do the first homework assignment.
+# Here's the first line of code to write to do the first homework assignment.
 #
 # In /scratch2/scratchdirs/nugent/astro250/hwk1 we have a set of fits files as well as catalogs
 # from these fits files. They are named the same save for the suffix (.fits or .cat). 
