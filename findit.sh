c378307fcf6dcfef4deaa33b6df9f03879aa7d0a
Peter Nugent <nugent68@berkeley.edu>
2017-02-13 09:56:01 -0800

    First homework assignment

--- findit.sh
+++ findit.sh
@@ -1,27 +1,14 @@
 #/bin/bash
 
-# Here's an example code to play with/re-write to do the first homework assignment.
+# Here's the first line of code to play write to do the first homework assignment.
 #
-# In /project/projectdirs/astro250/data/hwk1 we have a set of fits files as well as catalogs
+# In /scratch2/scratchdirs/nugent/astro250/hwk1 we have a set of fits files as well as catalogs
 # from these fits files. They are named the same save for the suffix (.fits or .cat). 
 # Some of the .cat files are missing. Write a bash shell script which takes as input 
 # the directory name and prints out the names of the .fits files which are missing their 
-# corresponding .cat files.
+# corresponding .cat files. If the directory does not exist, the code should print
+# out an error statement accordingly.
 #
 
 dir=$1
 
-file=junk.fits
-
-cd $dir
-
-for i in *.*
-do
-
-  if [[ $i == $file ]]; then
-    echo $file is in $dir
-  fi
-
-done
-
-cd ../
